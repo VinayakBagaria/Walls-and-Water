9d939e05218bf7ba014f10cb1f041da3d59996e0
VinayakBagaria <workvinayak95@gmail.com>
2018-04-25 14:03:01 +0530

    styles in style.css; nothing in scss - good ui now

--- script.js
+++ script.js
@@ -1,61 +1,61 @@
-let obj;
-document.getElementById('form').addEventListener('submit', e => {
-    e.preventDefault();
-    obj = [];
-    let values = document.getElementById('values').value.split(',');
-    values = [4, 0, 4];
+let obj = [];
+let values = document.getElementById('values').value.split(',');
+values = [0, 4, 0, 2, 0, 6, 0, 6, 4, 0, 2, 1];
 
-    const left = [];
-    const right = [];
+const left = [];
+const right = [];
 
-    values.map((item, index) => {
-        if (index === 0) {
-            left.push(parseInt(item));
-        } else {
-            left.push(Math.max(left[index - 1], parseInt(item)));
-        }
-    });
+values.map((item, index) => {
+    if (index === 0) {
+        left.push(parseInt(item));
+    } else {
+        left.push(Math.max(left[index - 1], parseInt(item)));
+    }
+});
 
-    values.reverse().map((item, index) => {
-        if (index === 0) {
-            right.push(parseInt(item));
-        } else {
-            right.push(Math.max(right[index - 1], parseInt(item)));
-        }
-    });
-    right.reverse();
+values.reverse().map((item, index) => {
+    if (index === 0) {
+        right.push(parseInt(item));
+    } else {
+        right.push(Math.max(right[index - 1], parseInt(item)));
+    }
+});
+right.reverse();
 
-    let wrapper = '';
+values = values.reverse();
 
-    const maxBar = Math.max(...values);
+const maxBar = Math.max(...values);
 
-    let waterUnits = 0;
-    for (let i = 0; i < values.length; i++) {
-        const wu = Math.min(left[i], right[i]) - values[i];
-        waterUnits += wu;
+document.getElementById('inputData').innerHTML = `[${values}]`;
 
-        wrapper += `<div class="block ${i + 1}">`;
+let wrapper = '';
 
-        for (let j = 0; j < values[i]; j++) {
-            wrapper += `
-                <div class="bar"></div>
-            `;
-        }
+let waterUnits = 0;
+for (let i = 0; i < values.length; i++) {
+    const wu = Math.min(left[i], right[i]) - values[i];
+    waterUnits += wu;
 
-        for (let j = 0; j < wu; j++) {
-            wrapper += `
-                <div class="water"></div>
-            `;
-        }
+    wrapper += `<div class="block ${i + 1}">`;
 
-        for (let j = 0; j < maxBar - values[i] - wu; j++) {
-            wrapper += `
+    for (let j = 0; j < maxBar - values[i] - wu; j++) {
+        wrapper += `
                 <div class="remaining"></div>
             `;
-        }
-
-        wrapper += '</div>';
     }
-    console.log(wrapper);
-    document.getElementById('container').innerHTML = wrapper;
-});
+
+    for (let j = 0; j < wu; j++) {
+        wrapper += `
+                <div class="water"></div>
+            `;
+    }
+
+    for (let j = 0; j < values[i]; j++) {
+        wrapper += `
+                <div class="bar"></div>
+            `;
+    }
+
+    wrapper += '</div>';
+}
+console.log(wrapper);
+document.getElementById('container').innerHTML = wrapper;
