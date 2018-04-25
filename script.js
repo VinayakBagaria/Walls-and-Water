2b71d36bac02c3f2af583e6d55eb91ca592df0ef
VinayakBagaria <workvinayak95@gmail.com>
2018-04-25 12:21:42 +0530

    made a demo sample, css needed to visualise

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('form').addEventListener('submit', e => {
     e.preventDefault();
     obj = [];
     let values = document.getElementById('values').value.split(',');
-    values = [0, 4, 0, 2, 0, 6, 0, 6, 4, 0];
+    values = [4, 0, 4];
 
     const left = [];
     const right = [];
@@ -25,9 +25,37 @@ document.getElementById('form').addEventListener('submit', e => {
     });
     right.reverse();
 
+    let wrapper = '';
+
+    const maxBar = Math.max(...values);
+
     let waterUnits = 0;
     for (let i = 0; i < values.length; i++) {
-        waterUnits += Math.min(left[i], right[i]) - values[i];
+        const wu = Math.min(left[i], right[i]) - values[i];
+        waterUnits += wu;
+
+        wrapper += `<div class="block ${i + 1}">`;
+
+        for (let j = 0; j < values[i]; j++) {
+            wrapper += `
+                <div class="bar"></div>
+            `;
+        }
+
+        for (let j = 0; j < wu; j++) {
+            wrapper += `
+                <div class="water"></div>
+            `;
+        }
+
+        for (let j = 0; j < maxBar - values[i] - wu; j++) {
+            wrapper += `
+                <div class="remaining"></div>
+            `;
+        }
+
+        wrapper += '</div>';
     }
-    console.log(waterUnits);
+    console.log(wrapper);
+    document.getElementById('container').innerHTML = wrapper;
 });
