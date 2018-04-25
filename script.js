e6fe5cbbde0568466720979be95b5ea362c89a1a
VinayakBagaria <workvinayak95@gmail.com>
2018-04-25 12:09:50 +0530

    indented with 4 space

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 let obj;
 document.getElementById('form').addEventListener('submit', e => {
-  e.preventDefault();
-  obj = [];
-  let values = document.getElementById('values').value.split(',');
-  values = [0, 4, 0, 2, 0, 6, 0, 6, 4, 0];
+    e.preventDefault();
+    obj = [];
+    let values = document.getElementById('values').value.split(',');
+    values = [0, 4, 0, 2, 0, 6, 0, 6, 4, 0];
 
-  const left = [];
-  const right = [];
+    const left = [];
+    const right = [];
 
-  values.map((item, index) => {
-    if (index === 0) {
-      left.push(parseInt(item));
-    } else {
-      left.push(Math.max(left[index - 1], parseInt(item)));
+    values.map((item, index) => {
+        if (index === 0) {
+            left.push(parseInt(item));
+        } else {
+            left.push(Math.max(left[index - 1], parseInt(item)));
+        }
+    });
+
+    values.reverse().map((item, index) => {
+        if (index === 0) {
+            right.push(parseInt(item));
+        } else {
+            right.push(Math.max(right[index - 1], parseInt(item)));
+        }
+    });
+    right.reverse();
+
+    let waterUnits = 0;
+    for (let i = 0; i < values.length; i++) {
+        waterUnits += Math.min(left[i], right[i]) - values[i];
     }
-  });
-
-  values.reverse().map((item, index) => {
-    if (index === 0) {
-      right.push(parseInt(item));
-    } else {
-      right.push(Math.max(right[index - 1], parseInt(item)));
-    }
-  });
-  right.reverse();
-
-  let waterUnits = 0;
-  for (let i = 0; i < values.length; i++) {
-    waterUnits += Math.min(left[i], right[i]) - values[i];
-  }
-  console.log(waterUnits);
+    console.log(waterUnits);
 });
